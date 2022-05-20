4e96be66bdf4bcce9d5e0afb2c7081bb7fc307d2
LeonardoScherer <leoblacena11@gmail.com>
2022-05-20 13:45:27 -0300

    Testando a criação de modal

--- 3_projeto_cli/src/App.vue
+++ 3_projeto_cli/src/App.vue
@@ -1,22 +1,54 @@
 <template>
-  <PrimeiroComponente />
-  <hr>
-  <LifeCycle />
-  <hr>
-  <Pessoa />
+  <div>
+    <PrimeiroComponente />
+    <hr>
+    <LifeCycle />
+    <hr>
+    <Pessoa />
+
+    <div class="my-3">
+      <DialogModal v-if="openDialog">
+        <div><h1>Download em andamento</h1></div>
+        <div>
+          <p><b>Atenção:</b> este é um teste de modal</p>
+        </div>
+        <div>
+          <button class="btn btn-primary" @click.prevent="openDialog = false">OK</button>
+        </div>
+      </DialogModal>
+        <div>
+          <button @click.prevent="onOpenDialog()" class="btn btn-primary">
+            Abrir dialogo
+          </button>
+        </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
   import LifeCycle from './components/LifeCycle.vue'
   import Pessoa from './components/Pessoa.vue'
+  import DialogModal from './components/DialogModal.vue'
 
   export default {
     name: 'App',
+    data() {
+      return {
+        openDialog: false,
+      }
+    },
     components: {
       PrimeiroComponente,
       LifeCycle,
-      Pessoa
-    }
+      Pessoa,
+      DialogModal
+    },
+    methods: {
+      onOpenDialog() {
+        this.openDialog = true;
+      }
+    },
   }
 </script>
---
+++ 3_projeto_cli/src/components/DialogModal.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="backdrop"></div>
+            
+        <div class="dialog">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.backdrop {
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+}
+.dialog {
+    position: fixed;
+    top: 20%;
+    background: #fff;
+    width: 60vw;
+    left: 20vw;
+    padding: 20px;
+}
+</style>
+
+<script>
+export default {
+    name: 'DialogModal'
+}
+</script>
