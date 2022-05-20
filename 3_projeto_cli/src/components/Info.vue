61959327649de5e2d24e993ea3a7f72636b26f20
LeonardoScherer <leoblacena11@gmail.com>
2022-05-20 10:30:39 -0300

    Hierarquia de componentes

---
+++ 3_projeto_cli/src/components/Info.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>Trabalhando no momento</p>
+        <p>Hard Skills:</p>
+        <ul>
+            <li>
+                PHP
+            </li>
+            <li>
+                JavaScript
+            </li>
+            <li>
+                MYSQL
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Info',   
+    }
+</script>
---
+++ 3_projeto_cli/src/components/Pessoa.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Informações sobre: {{ nome }}</h2>
+        <Info />
+        <Form />
+    </div>
+</template>
+
+<script>
+    import Form from './Form.vue'
+    import Info from './Info.vue'
+
+    export default {
+        components: { Info, Form },
+        name: 'Pessoa',
+        data() {
+            return {
+                nome: 'Creusa'
+            }
+        }
+    }
+</script>
