a2adb94dae2ebeed3fd0d84394f23d8e16cc5193
LeonardoScherer <leoblacena11@gmail.com>
2022-05-20 10:01:14 -0300

    Lifecycle hooks

---
+++ 3_projeto_cli/src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Nome: {{ nome }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LifeCycle',
+        data() {
+            return {
+                nome: 'Fulano'
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.nome = 'Ciclano'
+            }, 1000);
+        },
+        mounted() {
+            setTimeout(() => {
+                this.nome = 'Beltrano'
+            }, 1500);
+        }
+    }
+</script>
