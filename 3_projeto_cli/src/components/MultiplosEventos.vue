6ce7baa259e74a0d8f777abec10eb7ce98c24a85
LeonardoScherer <leoblacena11@gmail.com>
2022-05-21 16:15:53 -0300

    Múltiplos eventos

---
+++ 3_projeto_cli/src/components/MultiplosEventos.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <button @click="primeiro('Primeiro evento', $event), segundo($event), terceiro(txt3, event), quarto()">Ativar múltiplos métodos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MultiplosEventos',
+    data() {
+        return {
+            multiplos_eventos: "",
+            txt3: "Terceiro evento"
+        }
+    },
+    methods: {
+      primeiro(txt, event) {
+        this.multiplos_eventos = txt
+      },
+      segundo(event) {
+        setTimeout(() => this.multiplos_eventos = "Segundo evento", 1000)
+      },
+      terceiro(txt, event) {
+        setTimeout(() => this.multiplos_eventos = txt, 1500)
+      },
+      quarto() {
+        setTimeout(() => this.multiplos_eventos = "", 2000)
+      }
+    }
+
+}
+</script>
+
+<style scoped>
+    div {
+        margin: 100px;
+    }
+</style>
--- 3_projeto_cli/src/components/PrimeiroComponente.vue
+++ 3_projeto_cli/src/components/PrimeiroComponente.vue
@@ -2,12 +2,17 @@
     <div>
         <h1>Componente Vue 3</h1>
         <p>Meu nome é: {{ nome }} e trabalho como {{ profissao }}</p>
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+    import MultiplosEventos from './MultiplosEventos.vue'
     export default {
         name: 'PrimeiroComponente',
+        components: {
+            MultiplosEventos
+        },
         data() {
             return {
                 nome: 'Leonardo',
