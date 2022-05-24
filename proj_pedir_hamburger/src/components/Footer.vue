32b18ae43f9ecd2c38b80a0a8ad65922ef7810da
LeonardoScherer <leoblacena11@gmail.com>
2022-05-24 17:37:22 -0300

    Finalizando footer e header

--- proj_pedir_hamburger/src/components/Footer.vue
+++ proj_pedir_hamburger/src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>footer</p>
+    <div id="footer">
+        <p>Peça seu hamburger &copy; 2022</p>
+    </div>
 </template>
 
 <script>
@@ -7,3 +9,18 @@ export default {
     name: 'Footer'
 }
 </script>
+
+<style scoped>
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA04;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        bottom: 0;
+        width: 100%;
+        position: absolute;
+    }
+</style>
