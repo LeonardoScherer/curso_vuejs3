3caf00447abcdabc1728c7b6e24fffacedc5d667
LeonardoScherer <leoblacena11@gmail.com>
2022-05-25 14:59:41 -0300

    Mensagens nas atualizações e remoções de pedidos

--- proj_pedir_hamburger/src/App.vue
+++ proj_pedir_hamburger/src/App.vue
@@ -32,6 +32,7 @@ export default {
   .main-container {
     margin: 50px;
     min-height: 250px;
+    height: 720px;
   }
 
   h1 {
--- proj_pedir_hamburger/src/components/Dashboard.vue
+++ proj_pedir_hamburger/src/components/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="burger-table">
+        <Message :msg="msg" v-show="msg"/>
         <div>
             <div id="burger-table-heading">
                 <div class="order-id">#:</div>
@@ -34,14 +35,20 @@
 </template>
 
 <script>
+import Message from './Message.vue';
+
 export default {
     name: 'Dashboard',
     data() {
         return {
             burgers: null,
             burger_id: null,
-            status: []
+            status: [],
+            msg: null
         }
+    },
+    components: {
+        Message
     },
     methods: {
         async getPedidos() {
@@ -76,6 +83,10 @@ export default {
 
             const res = await req.json();
 
+            this.msg = `Pedido removido com sucesso`;
+
+            setTimeout(() => this.msg = "", 3000);
+
             this.getPedidos();
         },
 
@@ -93,6 +104,10 @@ export default {
 
             const res = await req.json();
 
+            this.msg = `Pedido Nº ${res.id} foi atualizado para ${res.status}`;
+
+            setTimeout(() => this.msg = "", 3000);
+            
             console.log(res);
         }
 
