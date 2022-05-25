cfabbf21aefb9005f907c88d2d14ca30288be90d
LeonardoScherer <leoblacena11@gmail.com>
2022-05-25 11:54:37 -0300

    Criando a tela de pedidos (Dashboard)

---
+++ proj_pedir_hamburger/src/components/Dashboard.vue
@@ -0,0 +1,99 @@
+<template>
+    <div id="burger-table">
+        <div>
+            <div id="burger-table-heading">
+                <div class="order-id">#:</div>
+                <div>Cliente:</div>
+                <div>Pão:</div>
+                <div>Carne:</div>
+                <div>Opcionais:</div>
+                <div>Ações:</div>
+            </div>
+        </div>
+        <div id="burger-table-rows">
+            <div class="burger-table-rows">
+                <div class="order-number">1</div>
+                <div>Fulano</div>
+                <div>Pão bla</div>
+                <div>Maminha</div>
+                <div>
+                    <ul>
+                        <li>Salame</li>
+                        <li>Tomate</li>
+                    </ul>
+                </div>
+                <div>
+                    <select name="status" id="status">
+                        <option value="">Selecione</option>
+                    </select>
+                    <button class="delete-btn">Cancelar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Dashboard'
+}
+</script>
+
+<style scoped>
+
+    #burger-table {
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+    #burger-table-heading,
+    #burger-table-rows,
+    .burger-table-rows {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    #burger-table-heading {
+        font-weight: bold;
+        padding: 12px;
+        border-bottom: 3px solid #333;
+    }
+
+    #burger-table-heading div,
+    .burger-table-rows div {
+        width: 19%;
+    }
+
+    .burger-table-rows {
+        width: 100%;
+        padding: 12px;
+        border-bottom: 1px solid #CCC;
+    }
+
+    #burger-table-heading .order-id,
+    .burger-table-rows .order-number {
+        width: 5%;
+    }
+
+    select {
+        padding: 12px 6px;
+        margin-right: 12px;
+    }
+
+    .delete-btn {
+        background-color: #222;
+        color: #FCBA03;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .delete-btn:hover {
+        background-color: transparent;
+        color: #222;
+    }
+
+</style>
